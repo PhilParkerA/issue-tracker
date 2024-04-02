a3b88721e19f21c88e7da9c4acf51e40d7237e9a
Philip Parker <philipparker1993@gmai.com>
2024-04-02 15:44:52 +0000

    Show Issue Details

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,28 @@
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <>
+      <div>{issue.title} </div>
+      <div>{issue.description} </div>
+      <div>{issue.status} </div>
+      <div>{issue.createdAt.toDateString()} </div>
+    </>
+  );
+};
+
+export default IssueDetailPage;
