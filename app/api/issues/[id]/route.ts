c8bd856bd19dc219110c5712f1860a36ab6bd263
Philip Parker <philipparker1993@gmai.com>
2024-04-04 16:44:54 +0000

    Build an API for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,33 @@
+import { createIssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { Issue } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  const { title, description } = body;
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) return NextResponse.json("Invalid Issue", { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title,
+      description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue, { status: 200 });
+}
