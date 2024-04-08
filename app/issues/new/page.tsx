02ddc167ba4dd67abda97fa372c9b8eebaba164e
Philip Parker <philipparker1993@gmai.com>
2024-04-08 14:51:36 +0000

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,10 @@
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
-import IssueForm from "../_components/IssueForm";
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const NewIssuePage = () => {
   return <IssueForm />;
