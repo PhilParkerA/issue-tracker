1883cd4039974d05c4d2c3605cb79ccda812163b
Philip Parker <philipparker1993@gmai.com>
2024-04-09 10:43:10 +0000

    Show information Dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 interface Props {
   issueId: number;
 }
 
 const DeleteIssueButton = ({ issueId }: Props) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth={"450px"}>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? THIS ACTION CANNOT BE
+          UNDONE.
+        </AlertDialog.Description>
+        <Flex mt={"4"} gap={"2"} justify={"end"}>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              No
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
